fc5a05d439f65d8cff0c5747b619e433e526b950
Thomas Bennett <tbennett@talend.com>
2020-02-13 12:45:48 +0100

    updated build.sh and tcli.xml to properly build Windows executable

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ mvn validate
 mvn test
 mvn clean compile assembly:single
 
-cp ./target/tcli-1.3.0-jar-with-dependencies.jar ./tcli.jar
+cp ./target/hvr-tcli-1.0.0-jar-with-dependencies.jar ./tcli.jar
 cat stub.sh tcli.jar > tcli && chmod 777 tcli
 ~/launch4j/launch4j ./tcli.xml
 
