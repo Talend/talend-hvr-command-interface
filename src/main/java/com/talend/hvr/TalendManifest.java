8609970391743907b48b584408c1ea37ece0776a
Thomas Bennett <tbennett@talend.com>
2020-02-13 12:01:05 +0100

    Initial commit

---
+++ src/main/java/com/talend/hvr/TalendManifest.java
@@ -0,0 +1,15 @@
+package com.talend.hvr;
+
+import java.util.Hashtable;
+
+public class TalendManifest {
+    private Hashtable<String, String> tables;
+
+    public Hashtable<String, String> getTables() {
+        return tables;
+    }
+
+    public void setTables(Hashtable<String, String> tables) {
+        this.tables = tables;
+    }
+}
