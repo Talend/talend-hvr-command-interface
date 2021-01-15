2ad30ad5f28ae24cdbc9e113a4931a966b7518ba
Bobby <me@example.com>
2021-01-15 12:02:11 -0500

    updated to use TMC API 2.2

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ cp ./target/hvr-tcli-1.0.0-jar-with-dependencies.jar ./tcli.jar
 cat stub.sh tcli.jar > tcli && chmod 777 tcli
 ~/launch4j/launch4j ./tcli.xml
 
-cp tcli ~/IntelliJ/tcli/
-cp tcli.exe ~/IntelliJ/tcli/
+cp tcli ~/VSCode/hvr-tcli/
+cp tcli.exe ~/VSCode/hvr-tcli/
